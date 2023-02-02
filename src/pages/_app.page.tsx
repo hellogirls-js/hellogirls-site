fdd6eea4c17805b336ff611150e40da0429a25a6
neeneemi <neeneemille@gmail.com>
2023-02-01 18:11:11 -0600

    feat: ✨ added favicon

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -2,6 +2,8 @@ import "@/styles/globals.scss";
 import type { AppProps } from "next/app";
 import Head from "next/head";
 
+import favicon from "../../public/favicon.ico";
+
 import { DarkModeProvider } from "context/DarkModeContext";
 
 export default function App({ Component, pageProps }: AppProps) {
@@ -11,7 +13,7 @@ export default function App({ Component, pageProps }: AppProps) {
         <title>welcome-!</title>
         <meta name="description" content="son @HELLOGlRLS's personal website" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href={favicon.src} />
       </Head>
       <DarkModeProvider>
         <Component {...pageProps} />
