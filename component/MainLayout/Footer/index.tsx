3ff7a547d1f6e36eac7a773b734ddc2c9e15368b
neeneemi <neeneemille@gmail.com>
2023-02-03 13:35:50 -0600

    feat: ✨ updated footer

--- component/MainLayout/Footer/index.tsx
+++ component/MainLayout/Footer/index.tsx
@@ -1,15 +1,40 @@
 import Link from "next/link";
+import { useContext } from "react";
 
 import styles from "./Footer.module.scss";
-import { useContext } from "react";
+
 import { DarkModeContext } from "context/DarkModeContext";
 
 export default function Footer() {
   const { colorTheme } = useContext(DarkModeContext);
   return (
     <footer className={`${styles.footer} ${styles[colorTheme]}`}>
-      created by son{" "}
-      <Link href="https://twitter.com/HELLOGlRLS">@HELLOGlRLS</Link>
+      <p>
+        created by son{" "}
+        <Link href="https://twitter.com/HELLOGlRLS">@HELLOGlRLS</Link>
+      </p>
+      <p>
+        this website uses{" "}
+        <Link href="https://nextjs.org/" target="_blank">
+          nextjs
+        </Link>{" "}
+        and is hosted on{" "}
+        <Link href="vercel.com/" target="_blank">
+          vercel
+        </Link>
+        . icons are provided by{" "}
+        <Link href="https://tabler-icons.io" target="_blank">
+          tabler icons
+        </Link>
+        .
+      </p>
+      <p>
+        special thanks to{" "}
+        <Link href="https://kiramekiatelier.github.io/" target="_blank">
+          kirameki atelier
+        </Link>{" "}
+        for reviving my creative spirit!
+      </p>
     </footer>
   );
 }
