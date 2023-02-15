b2623a16b73053b4ef9cb6726fc98a5cd411fde0
neeneemi <neeneemille@gmail.com>
2023-02-15 13:35:14 -0600

    fix: 🐛 updated page meta

--- component/MDXTemplate/PostInfo/index.tsx
+++ component/MDXTemplate/PostInfo/index.tsx
@@ -4,6 +4,7 @@ import dayjs from "dayjs";
 import styles from "../MDXLayout.module.scss";
 
 export default function PostInfo({ meta }: { meta: PostMeta }) {
+  console.log(meta);
   return (
     <div className={styles.postInfo}>
       <span>
@@ -11,7 +12,7 @@ export default function PostInfo({ meta }: { meta: PostMeta }) {
       </span>
       {meta.dateCreated && (
         <span>
-          on {dayjs(meta.dateCreated).format("MM/DD/YYYY h:mma")}{" "}
+          on {dayjs(meta.dateCreated).format("MM/DD/YYYY h:mma")}
           {meta.dateUpdated &&
             meta.dateCreated !== meta.dateUpdated &&
             `, last updated: ${dayjs(meta.dateUpdated).format(
