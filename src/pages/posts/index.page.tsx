f9d7d9cd9a126cf41f63ff451011d3a5c14b453e
neeneemi <neeneemille@gmail.com>
2023-03-02 14:43:10 -0600

    feat: ✨ added predictions

--- src/pages/posts/index.page.tsx
+++ src/pages/posts/index.page.tsx
@@ -15,6 +15,8 @@ export default function Posts(props: any) {
 
   const { data: posts } = useSWR("/api/readposts", fetcher);
 
+  posts.sort((a, b) => b.id - a.id);
+
   function Post({ post }: { post: PostMeta }) {
     return (
       <div className={styles.post}>
