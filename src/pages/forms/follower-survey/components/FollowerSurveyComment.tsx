ce735020744cdff272507d0eaf75f1f6e7def267
neeneemi <neeneemille@gmail.com>
2023-09-24 15:49:43 -0500

    fix: ✨ added security check for longer comments

--- src/pages/forms/follower-survey/components/FollowerSurveyComment.tsx
+++ src/pages/forms/follower-survey/components/FollowerSurveyComment.tsx
@@ -1,8 +1,11 @@
 import { Dispatch, SetStateAction } from "react";
+import { IconAlertTriangle } from "@tabler/icons-react";
+import { useMediaQuery } from "@mantine/hooks";
 
 import styles from "../../styles/Form.module.scss";
 
 import Textarea from "component/utility/TextArea";
+import Alert from "component/utility/Alert";
 
 export default function FollowerSurveyComment({
   isVisible,
@@ -15,6 +18,8 @@ export default function FollowerSurveyComment({
   setIsBot: Dispatch<SetStateAction<boolean>>;
   error?: FormError | null;
 }) {
+  const isDesktop = useMediaQuery("(min-width: 1000px)");
+
   return (
     <div
       className={styles.formPart}
@@ -24,6 +29,21 @@ export default function FollowerSurveyComment({
         <h3>do you have any thoughts before you go? :3</h3>
       </label>
       <Textarea name="comment" placeholder="hiiiiii" maxLength={2500} />
+      {error?.isBot && (
+        <Alert
+          icon={<IconAlertTriangle />}
+          type="warning"
+          style={{ width: isDesktop ? "40%" : "100%", margin: "10px 0" }}
+        >
+          Nice try, bot.
+        </Alert>
+      )}
+      <input
+        type="checkbox"
+        style={{ display: "none" }}
+        id="intro-security-check"
+        onChange={(e) => setIsBot((e.target as HTMLInputElement).checked)}
+      />
     </div>
   );
 }
