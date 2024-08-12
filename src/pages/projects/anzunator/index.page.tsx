9f66ad4f3d6dff95cd416042c106d9525c09aa98
hellogirls-js <neeneemille@gmail.com>
2024-08-12 13:15:01 -0500

    work so far

---
+++ src/pages/projects/anzunator/index.page.tsx
@@ -0,0 +1,126 @@
+import { useInterval, useListState } from "@mantine/hooks";
+import { IconAsterisk } from "@tabler/icons-react";
+import { AnimatePresence, motion } from "framer-motion";
+import Link from "next/link";
+import { useCallback, useEffect, useState } from "react";
+
+import Anzu from "./components/Anzu";
+import Content from "./components/Content";
+import styles from "./styles/Anzunator.module.scss";
+import { ANZU_REGULAR } from "./utility/images";
+
+async function getData(url: string): Promise<any> {
+  try {
+    const response = await fetch(url);
+    let data = await response.json();
+    if (data[0]) {
+      data = data.filter((d: any) => d.compliant === "TRUE");
+    }
+    return {
+      status: "success",
+      data,
+    };
+  } catch (error) {
+    console.error(error);
+    return {
+      status: "error",
+    };
+  }
+}
+
+function Sparkle({ x, y }: { x: number; y: number }) {
+  return (
+    <AnimatePresence>
+      <motion.span
+        className={styles.sparkle}
+        style={{
+          left: x,
+          top: y,
+        }}
+        initial={{ opacity: 0, scale: 0 }}
+        animate={{ opacity: Math.random(), scale: 1 }}
+        exit={{ opacity: 0, scale: 0 }}
+      >
+        <IconAsterisk size={8} color="white" />
+      </motion.span>
+    </AnimatePresence>
+  );
+}
+
+export default function Anzunator(props: any) {
+  const [vw, setVw] = useState<number>();
+  const [vh, setVh] = useState<number>();
+  const [image, setImage] = useState(ANZU_REGULAR);
+  const [sparklePlacements, sparklesHandler] = useListState<{
+    x: number;
+    y: number;
+  }>([]);
+  const addSparkleInterval = useCallback(() => {
+    if (vw && vh) {
+      const xCoord = Math.random() * vw;
+      const yCoord = Math.random() * vh;
+      sparklesHandler.append({ x: xCoord, y: yCoord });
+    }
+  }, [vw, vh]);
+
+  const removeSparkleInterval = useCallback(() => {
+    sparklesHandler.remove(0);
+  }, [sparklePlacements]);
+
+  const addSparkle = useInterval(addSparkleInterval, 1000);
+  const removeSparkle = useInterval(removeSparkleInterval, 1000);
+
+  useEffect(() => {
+    setVw(window.innerWidth);
+    setVh(window.innerHeight);
+  }, []);
+
+  useEffect(() => {
+    if (vw && vh && !addSparkle.active) {
+      addSparkle.start();
+    }
+  }, [vw, vh]);
+
+  useEffect(() => {
+    if (
+      sparklePlacements.length > Math.ceil((vw ?? 750) / 15) &&
+      !removeSparkle.active
+    ) {
+      removeSparkle.start();
+    }
+
+    if (sparklePlacements.length === 0) {
+      removeSparkle.stop();
+    }
+  }, [sparklePlacements]);
+
+  return (
+    <main className={styles.main}>
+      <div className={styles.container}>
+        <div className={styles.anzuContainer}>
+          <Anzu image={image} />
+        </div>
+        <Content setImage={setImage} enData={props.enData} />
+      </div>
+      <footer className={styles.credit}>
+        made by <Link href="https://hellogirls.info">son</Link>
+      </footer>
+      <div className={styles.sparkles}>
+        {sparklePlacements.map((coord) => (
+          <Sparkle key={`${vw}${vh}`} {...coord} />
+        ))}
+      </div>
+    </main>
+  );
+}
+
+export async function getServerSideProps() {
+  const TL_DATA_URL = "https://tl.data.ensemble.moe/en/characters.json";
+  const enData = await getData(TL_DATA_URL);
+  return {
+    props: {
+      title: "anzunator",
+      enData: enData.data,
+    },
+  };
+}
---
+++ src/pages/projects/anzunator/utility/images.ts
@@ -0,0 +1,7 @@
+export const ANZU_REGULAR =
+  "https://assets.hellogirls.info/misc/anzu_talking.png";
+export const ANZU_THINKING =
+  "https://assets.hellogirls.info/misc/anzu_thinking.png";
+export const ANZU_OH = "https://assets.hellogirls.info/misc/anzu_oh.png";
+export const ANZU_EXCITED =
+  "https://assets.hellogirls.info/misc/anzu_excited.png";
